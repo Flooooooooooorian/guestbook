d2a2fb25b2db5ca056db752c1686e94a72c9cba0
MoinMarcell <marcell.dechant@protonmail.com>
2023-02-25 20:25:08 +0100

    Create SecurityConfig

---
+++ backend/src/main/java/com/github/moinmarcell/backend/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.github.moinmarcell.backend.security;
+
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.multipart.support.MultipartFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        insertFilters(servletContext, new MultipartFilter());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+//        requestHandler.setCsrfRequestAttributeName(null);
+
+        return httpSecurity
+//                .csrf(csrf -> csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                        .csrfTokenRequestHandler(requestHandler))
+//                .sessionManagement(session ->
+//                        session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .csrf().disable()
+                .httpBasic()
+                .authenticationEntryPoint((request, response, authException) ->
+                        response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase()))
+                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(request -> !request.getRequestURI().contains("/api") &&
+                                request.getMethod().matches(HttpMethod.GET.name())).permitAll()
+                        .requestMatchers("/api/users/me").permitAll()
+                        .requestMatchers("/api/guestbook-entries").permitAll()
+                        .requestMatchers("/api/guestbook-entries/{id}").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/api/users/logout")
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                        .permitAll())
+                .build();
+    }
+
+}

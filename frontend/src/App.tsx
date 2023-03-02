50ccb92396c6115ffe30e14fa88d20afd64e15a8
MoinMarcell <marcell.dechant@proton.me>
2023-03-02 14:25:45 +0100

    Update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,30 @@ import {Route, Routes} from "react-router-dom";
 import EntryGallery from "./EntryGallery";
 import axios from "axios";
 import {GuestbookEntry} from "./GuestbookEntry";
+import GitHubRedirectPage from "./GitHubRedirectPage";
 
 function App() {
 
     const [entries, setEntries] = useState<GuestbookEntry[]>([])
 
-  useEffect(() => {
-    axios.get("/api/guestbook-entries")
-        .then((response) => {
-            setEntries(response.data);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-  });
+    useEffect(() => {
+        axios.get("/api/guestbook-entries")
+            .then((response) => {
+                setEntries(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
 
-  return (
-    <div className="App">
-      <Routes>
-        <Route path={"/"} element={<EntryGallery entries={entries} />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={"/"} element={<EntryGallery entries={entries}/>}/>
+                <Route path={"/users/oauth/github"} element={<GitHubRedirectPage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;

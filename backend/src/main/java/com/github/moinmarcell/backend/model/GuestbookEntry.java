38f77c73f0585d3bd49d7fc5a38c26ab2f061ad1
MoinMarcell <marcell.dechant@proton.me>
2023-03-02 14:27:26 +0100

    Update GuestbookEntry

--- backend/src/main/java/com/github/moinmarcell/backend/model/GuestbookEntry.java
+++ backend/src/main/java/com/github/moinmarcell/backend/model/GuestbookEntry.java
@@ -1,8 +1,13 @@
 package com.github.moinmarcell.backend.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.ZonedDateTime;
 
+@Document
 public record GuestbookEntry(
+        @Id
         String id,
         String title,
         String content,

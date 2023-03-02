1b6636c90d109efc79c93f0c84ca7df4c689939c
MoinMarcell <marcell.dechant@proton.me>
2023-03-02 14:42:17 +0100

    Update SecurityConfig

--- backend/src/main/java/com/github/moinmarcell/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/github/moinmarcell/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.github.moinmarcell.backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,8 +26,8 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
-                .permitAll()
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/guestbook-entries").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
